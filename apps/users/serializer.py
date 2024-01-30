95aa87e41a702c7b6c416dc3dba969d5038f1d53
SerStone <mishagoal752@gmail.com>
2024-01-30 13:24:31 +0200

    add python/django hw3

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -1,17 +1,12 @@
 from rest_framework import serializers
 
-from apps.auto_parks.serializers import AutoParkSerializer
+from apps.auto_parks.serializers import AutoParkWithOutCarsSerializer
 from apps.users.models import UserModel
 
 
-class UserAutoParkSerializer(AutoParkSerializer):
-    class Meta(AutoParkSerializer.Meta):
-        fields = ('id', 'user', 'name', 'created_at', 'updated_at')
-
-
 class UserSerializer(serializers.ModelSerializer):
-    auto_parks = UserAutoParkSerializer(many=True, read_only=True)
+    auto_parks = AutoParkWithOutCarsSerializer(many=True, read_only=True)
 
     class Meta:
         model = UserModel
-        fields = ('id', 'username', 'age', 'gender', 'created_at', 'updated_at', 'auto_parks')
+        fields = ('id', 'username', 'age', 'gender', 'updated_at', 'created_at', 'auto_parks')
